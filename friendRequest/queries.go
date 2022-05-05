5b642997b2899a976eda77296f2de2361d01bfba
naifr <nr1707337@qu.edu.qa>
2022-05-05 13:20:44 +0300

    fixed issue with join in get friend requests

--- friendRequest/queries.go
+++ friendRequest/queries.go
@@ -2,7 +2,7 @@ package friendrequest
 
 const (
 	CREATE_FRIEND_REQUEST = "INSERT INTO friend_requests(user_requesting_id, user_requested_id, created_at) VALUES($1, $2, NOW())"
-	GET_FRIEND_REQUESTS   = "select friend_requests.id, username, about, dp from friend_requests inner join users on user_requested_id=users.id where users.id = $1 and status_id = $2"
+	GET_FRIEND_REQUESTS   = "select friend_requests.id, username, about, dp from friend_requests inner join users on user_requesting_id=users.id where user_requested_id = $1 and status_id = $2"
 	ACCEPT_FRIEND_REQUEST = "update friend_requests set status_id = 1 where id=$1"
 	REJECT_FRIEND_REQUEST = "update friend_requests set status_id = 2 where id=$1"
 )
